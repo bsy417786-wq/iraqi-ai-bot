4feba92dde75d5509a3eaedbd0d9be00cad3581d
bsy417786-wq <bsy417786@gmail.com>
2026-03-20 07:46:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("💻 مساعد المبيعات الذكي (عباس حيدر)")
 st.markdown("مرحباً بك في محلنا! أنا عباس حيدر، حاضر لأي استفسار عن اللابتوبات.")
 
 # وضع المفتاح مباشرة لضمان التشغيل السريع
-api_key = st.secrets["GOOGLE_API_KEY"]
+api_key = st.secrets["AIzaSyCCJtpyUJ79Xa9xsb9pIWLlQDFkssUc_Zc"]
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
